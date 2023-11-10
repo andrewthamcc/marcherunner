c7d42321e90e414090246f28773e58fb54427e0e
Andrew Tham <andrewtham@andrews-mbp.lan>
2023-11-09 22:47:46 -0500

    logout

--- app/routes/auth.logout.tsx
+++ app/routes/auth.logout.tsx
@@ -1,13 +1,21 @@
-import type { ActionFunctionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
-import { authConfig, getSession, destroySession } from '~/auth/auth.server'
+import type { ActionFunctionArgs } from '@remix-run/node'
+import { destroySession, getSession } from '~/auth/auth.server'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const session = await getSession(request.headers.get('Cookie'))
-  const logoutURL = new URL(authConfig.logout)
+  const logoutURL = new URL(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/v2/logout`)
+  const env = process.env.NODE_ENV || 'development'
 
-  logoutURL.searchParams.set('client_id', authConfig.clientId)
-  logoutURL.searchParams.set('returnTo', authConfig.callback)
+  logoutURL.searchParams.set(
+    'client_id',
+    process.env.REACT_APP_AUTH0_CLIENT_ID as string
+  )
+  logoutURL.searchParams.set('returnTo', 
+    env === 'development'
+      ? (process.env.REACT_APP_LOGOUT_DEV as string)
+      : (process.env.REACT_APP_LOGOUT as string)
+  )
 
   return redirect(logoutURL.toString(), {
     headers: {
