736ffd04d9643c0020596f9e55e6daecbec96087
Andrew Tham <andrewtham@andrews-mbp.lan>
2023-11-09 20:34:13 -0500

    fix title cool favicon

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => [
   {
     charset: "utf-8",
-    title: "New Remix App",
+    title: "MarchéRunner",
     viewport: "width=device-width,initial-scale=1",
   },
 ];
